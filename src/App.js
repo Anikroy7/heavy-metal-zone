d25359f47095c8aabed0ceda058123bc477a79b9
Anikroy7 <roy645800@gmail.com>
2022-03-26 13:09:55 +0600

    shop component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <main className='shop-container'>
+            <section className='bg-info'>This is products</section>
+            <section className='bg-danger'>This is select</section>
+        </main>
+    );
+};
+
+export default Shop;
